16ca0cc05eeca94233f339e4389a84bee3ebc82a
Omar Ibrahim <omar.ibrahim@dst.com.bn>
2024-10-28 16:33:07 +0800

    📦 add soft delete package

--- adonisrc.ts
+++ adonisrc.ts
@@ -32,6 +32,7 @@ export default defineConfig({
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/lucid/database_provider'),
     () => import('@crm/common/providers/common_provider'),
+    () => import('adonis-lucid-soft-deletes/provider'),
   ],
 
   /*
